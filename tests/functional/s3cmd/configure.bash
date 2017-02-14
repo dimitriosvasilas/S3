723374374b263ea7fd7820e3559fe6b03a784dbe
Electra Chong <electra.chong@scality.com>
2017-02-13 17:47:30 -0800

    [squash] minor modification to s3cmd configure.bash

--- tests/functional/s3cmd/configure.bash
+++ tests/functional/s3cmd/configure.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get install -y -q  python-dateutil python-magic
 wget https://launchpad.net/ubuntu/+archive/primary/+files/s3cmd_1.6.1.orig.tar.gz
-tar -xf s3cmd_1.6.1.orig.tar.gz
-cd s3cmd-1.6.1
+tar -xf s3cmd_1.6.1.orig.tar.gz && \
+cd s3cmd-1.6.1 && \
 sudo python setup.py install
