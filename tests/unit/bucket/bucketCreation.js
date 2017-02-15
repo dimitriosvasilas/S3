98049581bf9d006225486e87c0a78db514903153
Nicolas Humbert <nicolas.humbert@live.fr>
2017-02-14 16:34:09 -0800

    bucketCreation correction and explanations

--- tests/unit/bucket/bucketCreation.js
+++ tests/unit/bucket/bucketCreation.js
@@ -15,9 +15,11 @@ const authInfo = makeAuthInfo('accessKey1');
 
 let normalBehaviorLocationConstraint = 'us-west-1';
 let specialBehaviorLocationConstraint = 'us-east-1';
+let itSkipIfNewConfig = it;
 if (config.locationConstraints) {
     normalBehaviorLocationConstraint = 'scality-us-west-1';
     specialBehaviorLocationConstraint = 'scality-us-east-1';
+    itSkipIfNewConfig = it.skip;
 }
 
 describe('bucket creation', () => {
@@ -57,8 +59,10 @@ describe('bucket creation', () => {
             });
         });
 
-        it('should return 409 if try to recreate in us-east-1 but without ' +
-            'usEastBehavior config set', done => {
+        // Skip if new configuration.json because we do not use
+        // usEastBehavior anymore
+        itSkipIfNewConfig('should return 409 if try to recreate in ' +
+            'us-east-1 but without usEastBehavior config set', done => {
             createBucket(authInfo, bucketName, headers,
             specialBehaviorLocationConstraint, !usEastBehavior, log, err => {
                 assert.strictEqual(err, errors.BucketAlreadyOwnedByYou);
